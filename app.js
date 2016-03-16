31dfd0fec6fc5b1acea12d9719bde9d3fad05f58
melkinsgit <margaret.elkins@gmail.com>
2016-03-16 15:05:05 -0500

    First commit for Garden

---
+++ app.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var MongoClient = require('mongodb').MongoClient;
+var engines = require ('jade');
+var assert = require ('assert');
+
+app = express();
+
+app.set ('view engine', 'jade');
+app.set ('views', __dirname + "/views");
+
+// attempt to connect to MongoDB
+MongoClient.connect("mongodb://localhost:27017/garden", function (err,db){
+	assert.equal(null, err);  // crashes if error not null
+	console.log("Connected to to MongoDB");
+	
+	// routes - this is for the home page
+	app.get('/', function (req, res){
+		db.collection('flowers').find({}, {"name":true, "color":true}).toArray(function(err, docs){
+			if (err) {return res.sendStatus(500);}
+			return res.render('allflowers', {"flowers": docs});
+		});
+	});
+	
+	// all other requests, return 404 not found
+	app.use (function(req, res){
+		res.sendStatus(404);
+	});
+	
+	// and start the server on any port you like
+	var server = app.listen(3000, function(){
+		var port = server.address().port;
+		console.log("Server listening on port " + port);
+	});
+	
+});
